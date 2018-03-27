646a688098faef2af0dec50d668be292cd6d90f8
apichatthongngoen <37826468+apichatthongngoen@users.noreply.github.com>
2018-03-27 16:12:04 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,7 @@
 <?php
-$access_token = 'Q5FDv45xqC2HElUtYAoKpYX6P9VCHXEf2HRO2eWnt7Yh+GWRdA3/uzmEr2xH6e2f2uW+9hOC9IdWSfrgcGMpSzZq6ZkgC2pIz+0eDvceP19jMirXWr/wVmaK6QyldIc37w9VFC8KJy5bsFDAKawbEQdB04t89/1O/w1cDnyilFU=';
+$access_token = '
+Q5FDv45xqC2HElUtYAoKpYX6P9VCHXEf2HRO2eWnt7Yh+GWRdA3/uzmEr2xH6e2f2uW+9hOC9IdWSfrgcGMpSzZq6ZkgC2pIz+0eDvceP19jMirXWr/wVmaK6QyldIc37w9VFC8KJy5bsFDAKawbEQdB04t89/1O/w1cDnyilFU=
+';
 $url = 'https://api.line.me/v1/oauth/verify';
 $headers = array('Authorization: Bearer ' . $access_token);
 $ch = curl_init($url);
